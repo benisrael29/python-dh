98f141793208b103b7ca742bf5d66a21c4fd30ad
bisrael <bisrael@deloitte.com.au>
2024-07-14 19:18:49 +1000

    Introducing parallel processing

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import yfinance as yf
 import pandas as pd
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 ########## Constants ##########
-PERIOD = "max"
-LOW_PERIOD = 30
+PERIOD = "max" # The period to consider for the historical data
+LOW_PERIOD = 30 # Number of days to consider for the lowest Close price
+
+MAX_WORKERS = 30 # Number of threads to run in parallel
 
 ########## Screens ##########
 
@@ -18,26 +21,30 @@ def get_all_stocks():
     return tickers
 
 
-def run_screen(stock):
+def run_screen(stock_symbol):
     """
     This function checks if a stock meets certain criteria:
     - The highest Close price is at least double the lowest Close price.
     - The lowest Close price occurred in the last LOW_PERIOD days.
 
     Args:
-        stock (yfinance.Ticker): The stock to screen.
+        stock_symbol (str): The stock symbol to screen.
 
     Returns:
-        int: 1 if the stock meets the criteria, otherwise 0.
+        str: The stock symbol if it meets the criteria, otherwise None.
     """
+    stock = yf.Ticker(stock_symbol)
     hist = stock.history(period=PERIOD)
+    if hist.empty:
+        return None
+
     high = hist["Close"].max()
     low = hist["Close"].min()
     low_period_low = hist["Close"].nlargest(LOW_PERIOD).min()
-    print('Stock:', stock.ticker, 'High:', high, 'Low Period Low:', low_period_low)
+
     if high >= low * 2 and hist["Close"].idxmin() > hist.index[-LOW_PERIOD]:
-        return 1
-    return 0
+        return stock_symbol
+    return None
 
 
 ########## Main ##########
@@ -48,15 +55,20 @@ stocks = get_all_stocks()
 # Append .AX to the stock names to comply with Yahoo Finance
 stocks = [stock + ".AX" for stock in stocks]
 
-# Filter stocks based on the screening criteria
+print(f"Total stocks to process: {len(stocks)}")
+
+# Use ThreadPoolExecutor to run the screen in parallel
 buys = []
-for stock_symbol in stocks:
-    stock = yf.Ticker(stock_symbol)
-    if run_screen(stock) == 1:
-        buys.append(stock)
-        
+with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+    future_to_stock = {executor.submit(run_screen, stock): stock for stock in stocks}
+    for i, future in enumerate(as_completed(future_to_stock), 1):
+        result = future.result()
+        if result is not None:
+            buys.append(result)
+        print(f"Processed {i}/{len(stocks)} stocks")
+
 # Format the list of buys
-buys = ['ASX:' + stock.ticker.replace('.AX', '') for stock in buys]
+buys = ['ASX:' + stock.replace('.AX', '') for stock in buys]
 
 # Convert the list to a single string with comma separation
 buys_string = ','.join(buys)
@@ -65,4 +77,4 @@ buys_string = ','.join(buys)
 with open('output/watchlist.txt', 'w') as file:
     file.write(buys_string)
 
-print(buys_string)
+print(f"Buy list: {buys_string}")
